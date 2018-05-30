763fa0186eca16c567c2c8480a43b1b12439e514
antoniolourenco <36620695+antoniolourenco@users.noreply.github.com>
2018-05-30 09:10:51 +0100

    Update script2.js

--- 2/script2.js
+++ 2/script2.js
@@ -6,34 +6,99 @@ var raioBola = 10; //Raio da bola
 //Posicao x e y da bola
 var x = tela.width/2;
 var y = tela.height-30;
-//Velocidade x e y da bola
+
+//Aceleracao x e y da bola
 var dx = 2;
 var dy = -2;
+
+//Propriedades da base
+var baseAltura = 10;
+var baseLargura = 75;
+var baseX = (tela.width-baseLargura)/2;
+
+//Guarda se o utilizador está a clicar numa tecla
+var setaDireita = false;
+var setaEsquerda = false;
+
+//Adiciona event listeners às teclas pressionadas
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+//Verifica se parou de clicar numa tecla
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {
+		setaDireita= true;
+	}
+	else if(e.keyCode == 37) {
+		setaEsquerda= true;
+	 }
+}
+//Verifica se comecou a clicar em alguma tecla
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {
+		setaDireita= false;
+	}
+	else if(e.keyCode == 37) {
+		setaEsquerda= false;
+	}
+}
 
 //Funcao que desenha a bola
 function desenharBola() {
     ctx.beginPath(); //Comeca a desenhar
-    ctx.arc(x, y, 10, 0, Math.PI*2); //Desenha o circulo
+    ctx.arc(x, y, raioBola, 0, Math.PI*2); //Desenha o circulo
     ctx.fillStyle = "#0095DD"; //Cor do circulo
     ctx.fill(); //Preenche o circulo
     ctx.closePath(); //Finaliza o circulo
+}
+
+//Funcao que desenha a base
+function desenharBase() {
+	ctx.beginPath();
+	ctx.rect(baseX, tela.height-baseAltura, baseLargura, baseAltura);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
 }
 
 //Funcao que desenha na tela
 function desenhar() {
 	ctx.clearRect(0, 0, tela.width, tela.height); //Limpa a tela
 	desenharBola(); //Chama a funcao que desenha a bola
-	
-	//Colisões da bola com a tela
+	desenharBase(); //Chama a funcao que desenha a base
+
+//Colisões da bola com a tela
 	if(x + dx > tela.width-raioBola || x + dx < raioBola) {
 		dx = -dx;
 	}
-	if(y + dy > tela.height-raioBola || y + dy < raioBola) {
+	if(y + dy < raioBola){
 		dy = -dy;
 	}
 
+	//Colisão bola com a base
+	else if(y + dy > tela.height-raioBola) {
+		if(x > baseX && x < baseX+ baseLargura) {
+			dy = -dy;
+		}
+		//Caso nao apanhe a bola
+		else {
+			clearInterval(game); //Acaba o jogo
+			alert("Perdeste! Tenta novamente e tens um presente"); //Mensagem
+			document.location.reload();
+		}
+	}
+
+	//Move a base
+	if(setaDireita && baseX < tela.width-baseLargura) {
+		baseX += 7;
+	}
+	else if(setaEsquerda && baseX > 0) {
+		baseX -= 7;
+	}
+	
+	//Move a bola
 	x += dx;
 	y += dy;
 }
 
-setInterval(desenhar, 10);
+var game = setInterval(desenhar, 10); //Desenha na tela a cada 10 ms
